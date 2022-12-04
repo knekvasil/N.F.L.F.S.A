63ac2bd3cae6a2bad44c7a1c1e99e1b9c1e33787
knekvasil <kajnekvasil@gmail.com>
2022-12-03 19:09:46 -0500

    implemented grab_fixtures_by_week(), init get_fixture_data()

--- fantasy_scraper.py
+++ fantasy_scraper.py
@@ -1,38 +1,63 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL = "https://www.pro-football-reference.com/years/"
-# page = requests.get(URL)
-
-# soup = BeautifulSoup(page.content, "html.parser")
-
 class FantasyScraper:
-    base_url = "https://www.pro-football-reference.com/years/"
+    base_url = "https://www.pro-football-reference.com/"
 
     def __init__(self, years):
         self.years = years
+            
+    def init_soup(self, site_url: str) -> None:
+    	if type(site_url) != str:
+    		raise TypeError("Params: site_url must be of type str")
+    		
+    	page = requests.get(site_url)
+    	soup = BeautifulSoup(page.content, "html.parser")
+    	# Crop page to only include divs with relevant data
+    	cropped_page = soup.find(id="content")
+    	
+    	return cropped_page
+    	
+
+    def get_fixture_data(self, fixture_id: str):
+    	if type(fixture_id) != str:
+    		raise TypeError("Params: fixture_id must be of type str")
+    		
+    	fixture_url = f"{self.base_url}boxscores/{fixture_id}.htm"
+    	page = self.init_soup(fixture_url)
+    	
+    	# TODO: Grab + store fixture data
+    	
+    	return page
+
     
-    def print_years(self):
-        for year in self.years:
-            print(year)
+    def get_fixtures_by_week(self, week: str, year: str) -> list:
+    	if type(week) != str or type(year) != str:
+    		raise TypeError("Params: week, year must be of type str") 
+    		
+    	week_url = f"{self.base_url}years/{year}/week_{week}.htm"
+    	page = self.init_soup(week_url)
+    	
+    	fixture_summaries = page.find_all("td", class_="right gamelink")
+    	
+    	fixture_ids = list()
+    	for fixture_summary in fixture_summaries:
+    		fixture_id = fixture_summary.find("a")
+    		fixture_ids.append(fixture_id['href'][11:-4])
+    		
+    	return fixture_ids
 
-    def scan_matchup(self):
-        return 0
-
-    def scan_year(self):
-        return 0
 
 def main():
     years = ["2019"]
     scraper = FantasyScraper(years)
-    scraper.print_years()
+    
+    # Testing:
+    # print(scraper.get_fixtures_by_week("1", "2019"))
+    # print(scraper.get_fixture_data("/boxscores/201909080min.htm"))
+
 
 if __name__ == "__main__":
     main()
 
-
-
-
-# results = soup.find(id="wrap")
-# job_elements = results.find_all("td", class_="right gamelink")
 

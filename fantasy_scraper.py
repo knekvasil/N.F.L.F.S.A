c7f37bd730475a5b39f4ad3384eb9867ba43ee8a
knekvasil <kajnekvasil@gmail.com>
2022-12-13 22:30:19 -0500

    Added comments, separated team_stats, play-by-play tables for separate parsing

--- fantasy_scraper.py
+++ fantasy_scraper.py
@@ -31,6 +31,9 @@ class FantasyScraper:
     	off_player_table = cropped_page.find(id="all_player_offense")
     	off_player_table_body = off_player_table.find("tbody")
     	
+  		# Two tables require unique parsing: team_stats and play-by-play
+  		# table_fragments[4] = team_stats (Tag: wherever)
+  		# table_fragments[18] = play-by-play (Comment: post-table_fragments)
     	
     	# Grab all Comments on page
     	fixture_table_fragments = cropped_page.find_all(string=lambda text: isinstance(text, Comment))
@@ -46,20 +49,30 @@ class FantasyScraper:
     				continue	
     	
     	# Filter out non-relevant tables (including PBP: 18)
-    	filter_indices = [5,6,7,8,9,10,11,14,15,16]
+    	filter_indices = [4,5,6,7,8,9,10,11,14,15,16]
     	
     	# Insert Tag table grabbed earlier to filtered table fragments list
     	filtered_tables = [off_player_table_body] + [table_fragments[i] for i in filter_indices]
     	
     	player_store = dict()
-    	
+    	print(filtered_tables[1])
     	for table_fragment in filtered_tables:
     		table_data = self.get_table_data(table_fragment)
+    		print(table_data)
     		# Merging dicts with embedded keys is not trivial. Let a package do it for us instead.
+    		# BUG: Fails for players with the same name. e.g. Josh Allen, QB (Buffalo) vs. Josh Allen, DB (Jacksonville)
     		merge(player_store, table_data)
     		
     	# Testing
-    	print(json.dumps(player_store, indent=2))
+    	# print(json.dumps(player_store, indent=2))
+    	
+    	# Parse Play-by-play
+    	# pbp_data = self.get_table_data(table_fragments[18]) # This works but we need all players involved.
+    	                                                      # Searching by HTML tag <a> seems a lot simpler/faster
+    	                                                      # than regex searching through strings... 
+    	                                                      # Or we could just regex, idc.
+    	# print(json.dumps(pbp_data, indent=2))
+    	# self.parse_play_by_play(pbp_data)
     	return 0
 		
     
@@ -79,12 +92,12 @@ class FantasyScraper:
     	return fixture_ids
     
     
-    # Get all player stats by grouping (generic table)
+    # Get all player stats by grouping
     def get_table_data(self, table_fragment: "bs4.element") -> dict:    	
     	if type(table_fragment).__name__ == "Comment":
     		# Parse Comment back into Tag
     		table_soup = BeautifulSoup(table_fragment, "html.parser")
-    		player_table_rows = table_soup.findAll("tr")
+    		player_table_rows = table_soup.find_all("tr")
     	else:
     		# table_fragment is a Tag
     		player_table_rows = table_fragment.find_all("tr")
@@ -103,14 +116,21 @@ class FantasyScraper:
   
     	return player_store
   		
-    	
+  
     # A lot of required player data is not given upfront.
     # The simplest solution is to loop through the play-by-play feed and look for cases manually
     # This could get ugly...
-    def get_play_by_play(self, cropped_page: "bs4.element.Tag") -> dict:
+    def parse_play_by_play(self, play_store: "bs4.element.Tag") -> dict:
+    	# Kicking: All fieldgoals w/ yardage + outcome
     	return 0
     	
-    
+    	
+    # Due to the structure of the table, a different parsing appreach is required
+    # Try to modularize with get_table_data() after if possible
+    def parse_team_stats_table(self, table_fragment: "bs4.element.Tag") -> dict:
+    	return 0
+    	
+    	
 def main():
     years = ["2019"]
     scraper = FantasyScraper(years)
